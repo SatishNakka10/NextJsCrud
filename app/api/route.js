6d6cfec8b255b7eebfef48ee7a4a3d9e6edb443f
SatishNakka <nakkasatish619>
2025-09-29 17:52:04 +0530

    first commit

---
+++ app/api/route.js
@@ -0,0 +1,22 @@
+import { ConnectDB } from "@/lib/config/db";
+import TodoModel from "@/lib/config/models/TodoModel";
+import { NextResponse } from "next/server";
+
+const LoadDB=async()=>{
+    await ConnectDB();
+}
+
+LoadDB();
+
+export async function GET(request){
+    return NextResponse.json({msg:"get method hit"})
+}
+
+export async function POST(request){
+    const {title,description}=await request.json()
+    await TodoModel.create({
+        title,
+        description
+    })
+    return NextResponse.json({msg:"TODO Created"})
+}
